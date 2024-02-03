8632f50b2740776ab3b08ced3748a76765f5a8dc
Ndukwe Chima <chimandukwe.r@gmail.com>
2024-02-03 20:02:02 +0100

    first commit

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -49,6 +49,18 @@ const ContactPage = () => {
             </form>
           </div>
         </div>
+        <section className="">
+          <div className="">
+            <iframe 
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3974.0387933036122!2d7.352199174124972!3d5.097430338092376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10429bd39c2683db%3A0xb8a19dc41b834ca2!2sAba-Owerri%20Rd%2C%20Aba%2C%20Abia!5e0!3m2!1sen!2sng!4v1705585565730!5m2!1sen!2sng"
+              style={{ border: "0" }}
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+              className="w-25 h-75"
+            ></iframe>
+          </div>
+        </section>
       </div>
       <Footer />
     </>
